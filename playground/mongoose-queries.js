cb0a3fbbe84fe0c5e248eb8d37735464b348b076
Troy David Cook <www.troydavidcook@gmail.com>
2017-10-10 16:19:27 -0400

    Minor changes to mongoose-query file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -8,38 +8,43 @@ const {User} = require('./../server/models/user');
 var id = '59dbc24a9852534ff668b475';
 var userId = '59d94cef803d220b6c85e823';
 
-if (!ObjectId.isValid(id)) {
-  console.log('Not a valid ID');
+// if (!ObjectId.isValid(id)) {
+//   console.log('Not a valid ID');
+// }
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     console.log('ID not found.');
+//   } else {
+//     console.log('Todo', todo);
+//   }
+// }).catch((err) => {
+//   console.log("Sorry, couldn't find the id", err);
+// });
+//
+
+if (!ObjectId.isValid(userId)) {
+  return console.log('Sorry, User Id is not valid.');
 }
-
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log('Todos', todos);
-});
-
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo', todo);
-});
-
-Todo.findById(id).then((todo) => {
-  if (!todo) {
-    console.log('ID not found.');
-  } else {
-    console.log('Todo', todo);
-  }
-}).catch((err) => {
-  console.log("Sorry, couldn't find the id", err);
-});
 
 User.findById(userId).then((user) => {
   if (!user) {
-    return console.log('Unable to find user.');
+    return console.log('Sorry, user not found');
   } else {
-    console.log('User', user);
+    console.log('User info: ', (JSON.stringify(user, undefined, 2)));
   }
 }).catch((err) => {
-  console.log('Unable to find that ID.', err);
+  console.log('Error Occured', err);
 });
